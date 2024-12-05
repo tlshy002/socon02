a5371a691922d883d7077baa25d0a48b2baeed32
tlshy002 <tlshy002@gmail.com>
2024-12-05 15:01:46 +0900

    버튼 카운트기능과 사진띄우기 deploy

--- src/App.js
+++ src/App.js
@@ -1,33 +1,56 @@
 // import './App.css';
+import React from 'react';
 
 function Food({name, picture}) {
   return (
     <div>
-      <h1>I love {name}</h1>
-      <img src={picture} />
+      <h2>I love {name}</h2>
+      <img src={picture} width={300} />
     </div>
   )
 }
-
 const foodILike = [
   {
      name: "apple",
-     image: "http://www.foodnmed.com/news/photo/201906/18597_4291_812.jpg",
+     image: "https://www.outdoornews.co.kr/news/photo/202009/32077_90504_551.jpg",
     },
     {
-      name: "banana",
-      image: "http://www.foodnmed.com/news/photo/201906/18597_4291_812.jpg",
+      name: "mango",
+      image: "https://health.chosun.com/site/data/img_dir/2024/04/09/2024040901881_0.jpg",
     },
 ];
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      { foodILike.map(dish => (
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  
+  add = () => {
+    console.log('add');
+    this.setState(current => ({
+      count: current.count + 1,
+    }));
+  }
+  minus = () => {
+    console.log('minus');
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  }
+  
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button><hr></hr>
+
+        <h1>Hello</h1>
+        { foodILike.map(dish => (
         <Food name={dish.name} picture={dish.image} />) ) }
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
